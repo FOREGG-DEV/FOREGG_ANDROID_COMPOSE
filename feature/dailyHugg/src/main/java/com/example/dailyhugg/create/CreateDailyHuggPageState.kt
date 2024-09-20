4e26546a8e4ce596b957aeb99e561830d40d652c
dogmania <hj296914@gmail.com>
2024-09-21 01:14:27 +0900

    :sparkles: Feat: CreateDailyHuggPageState 구현

--- feature/dailyHugg/src/main/java/com/example/dailyhugg/create/CreateDailyHuggPageState.kt
+++ feature/dailyHugg/src/main/java/com/example/dailyhugg/create/CreateDailyHuggPageState.kt
@@ -1,16 +1,19 @@
 package com.example.dailyhugg.create
 
-import androidx.compose.ui.graphics.Color
+import android.net.Uri
 import com.hugg.domain.model.enums.DailyConditionType
 import com.hugg.feature.base.PageState
-import com.hugg.feature.theme.InActiveMainNormal
-import com.hugg.feature.theme.MainNormal
 
 data class CreateDailyHuggPageState(
     val dailyHuggContent: String = "",
-    val dailyConditionType: DailyConditionType = DailyConditionType.DEFAULT
+    val dailyConditionType: DailyConditionType = DailyConditionType.DEFAULT,
+    val selectedImageUri: Uri? = null
 ): PageState {
-    val clickable: Boolean = if (dailyHuggContent.isEmpty() || dailyConditionType == DailyConditionType.DEFAULT) false else true
-    val btnRegistrationColor: Color
-        get() = if (clickable) MainNormal else InActiveMainNormal
+    val clickable: Boolean =
+        if (
+            dailyHuggContent.isEmpty() ||
+            dailyConditionType == DailyConditionType.DEFAULT ||
+            selectedImageUri == null
+        ) false
+        else true
 }
