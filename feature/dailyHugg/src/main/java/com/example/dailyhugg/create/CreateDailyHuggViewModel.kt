7a135b1f793a83a5fcc6030f7e888200be4e9b29
dogmania <hj296914@gmail.com>
2024-09-23 11:35:46 +0900

    :sparkles: Feat: 데일리 허그 생성 API 연결

--- feature/dailyHugg/src/main/java/com/example/dailyhugg/create/CreateDailyHuggViewModel.kt
+++ feature/dailyHugg/src/main/java/com/example/dailyhugg/create/CreateDailyHuggViewModel.kt
@@ -1,14 +1,24 @@
 package com.example.dailyhugg.create
 
 import android.net.Uri
+import androidx.lifecycle.viewModelScope
+import com.google.gson.Gson
 import com.hugg.domain.model.enums.DailyConditionType
+import com.hugg.domain.model.request.dailyHugg.CreateDailyHuggVo
+import com.hugg.domain.model.vo.dailyHugg.CreateDailyHuggDto
+import com.hugg.domain.repository.DailyHuggRepository
 import com.hugg.feature.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.toRequestBody
+import java.net.URL
 import javax.inject.Inject
 
 @HiltViewModel
 class CreateDailyHuggViewModel @Inject constructor(
-
+    private val dailyHuggRepository: DailyHuggRepository
 ) : BaseViewModel<CreateDailyHuggPageState>(CreateDailyHuggPageState()) {
     fun onDailyHuggContentChange(newContent: String) {
         updateState(
@@ -32,5 +42,22 @@ class CreateDailyHuggViewModel @Inject constructor(
                 selectedImageUri = uri
             )
         )
+    }
+
+    fun createDailyHugg(image: MultipartBody.Part?) {
+        val gson = Gson()
+        val dtoJson = gson.toJson(CreateDailyHuggDto(
+            dailyConditionType = uiState.value.dailyConditionType,
+            content = uiState.value.dailyHuggContent
+        ))
+        val dtoRequestBody = dtoJson.toRequestBody("application/json".toMediaTypeOrNull())
+        viewModelScope.launch {
+            dailyHuggRepository.createDailyHugg(
+                image = image!!,
+                dto = dtoRequestBody
+            ).collect {
+                resultResponse(it, {})
+            }
+        }
     }
 }
