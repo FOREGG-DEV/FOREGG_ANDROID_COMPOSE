4138b637a2c28ae6a3c9add9253172990cf7b556
dogmania <hj296914@gmail.com>
2024-09-23 11:25:13 +0900

    :sparkles: Feat: 서버 통신 data class 수정

--- domain/src/main/java/com/hugg/domain/model/request/dailyHugg/CreateDailyHuggVo.kt
+++ domain/src/main/java/com/hugg/domain/model/request/dailyHugg/CreateDailyHuggVo.kt
@@ -1,8 +1,9 @@
 package com.hugg.domain.model.request.dailyHugg
 
 import com.hugg.domain.model.vo.dailyHugg.CreateDailyHuggDto
+import okhttp3.MultipartBody
 
 data class CreateDailyHuggVo(
-    val image: String,
+    val image: MultipartBody.Part,
     val dto: CreateDailyHuggDto
 )
