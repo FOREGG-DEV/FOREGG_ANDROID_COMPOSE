e48f8e95d5732c2f4c3e345ec9be934a0b154d95
dogmania <hj296914@gmail.com>
2024-10-05 21:56:47 +0900

    :sparkles: Feat: PageState 구현

--- feature/dailyHugg/src/main/java/com/hugg/dailyhugg/all/DailyHuggListPageState.kt
+++ feature/dailyHugg/src/main/java/com/hugg/dailyhugg/all/DailyHuggListPageState.kt
@@ -1,7 +1,8 @@
 package com.hugg.dailyhugg.all
 
+import com.hugg.domain.model.response.dailyHugg.DailyHuggListItemVo
 import com.hugg.feature.base.PageState
 
 data class DailyHuggListPageState(
-    val temp: String = ""
+    val dailyHuggList: List<DailyHuggListItemVo> = emptyList()
 ): PageState
