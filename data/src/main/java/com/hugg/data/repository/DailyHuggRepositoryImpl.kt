1d27d0045501f04b1bb47461259bb6691ed738a7
dogmania <hj296914@gmail.com>
2024-10-14 01:33:35 +0900

    :sparkles: Feat: 데일리 허그 전체 조회 Repository 구현

--- data/src/main/java/com/hugg/data/repository/DailyHuggRepositoryImpl.kt
+++ data/src/main/java/com/hugg/data/repository/DailyHuggRepositoryImpl.kt
@@ -3,10 +3,12 @@ package com.hugg.data.repository
 import com.hugg.data.api.DailyHuggApi
 import com.hugg.data.base.BaseRepository
 import com.hugg.data.mapper.UnitResponseMapper
+import com.hugg.data.mapper.dailyHugg.DailyHuggListResponseMapper
 import com.hugg.data.mapper.dailyHugg.DailyHuggResponseMapper
 import com.hugg.domain.base.ApiState
 import com.hugg.domain.model.request.dailyHugg.CreateDailyHuggVo
 import com.hugg.domain.model.response.dailyHugg.DailyHuggItemVo
+import com.hugg.domain.model.response.dailyHugg.DailyHuggListResponseVo
 import com.hugg.domain.model.vo.dailyHugg.CreateDailyHuggDto
 import com.hugg.domain.repository.DailyHuggRepository
 import kotlinx.coroutines.flow.Flow
@@ -38,5 +40,9 @@ class DailyHuggRepositoryImpl @Inject constructor(
 
     override suspend fun deleteDailyHugg(id: Long): Flow<ApiState<Unit>> {
         return apiLaunch(apiCall = { dailyHuggApi.deleteDailyHugg(id = id) }, UnitResponseMapper)
+    }
+
+    override suspend fun getDailyHuggList(page: Int): Flow<ApiState<DailyHuggListResponseVo>> {
+        return apiLaunch(apiCall = { dailyHuggApi.getDailyHuggList(page = page) }, DailyHuggListResponseMapper)
     }
 }
--- domain/src/main/java/com/hugg/domain/repository/DailyHuggRepository.kt
+++ domain/src/main/java/com/hugg/domain/repository/DailyHuggRepository.kt
@@ -2,6 +2,7 @@ package com.hugg.domain.repository
 
 import com.hugg.domain.base.ApiState
 import com.hugg.domain.model.response.dailyHugg.DailyHuggItemVo
+import com.hugg.domain.model.response.dailyHugg.DailyHuggListResponseVo
 import kotlinx.coroutines.flow.Flow
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
@@ -11,4 +12,5 @@ interface DailyHuggRepository {
     suspend fun getDailyHuggByDate(date: String): Flow<ApiState<DailyHuggItemVo>>
     suspend fun editDailyHugg(id: Long, image: MultipartBody.Part, dto: RequestBody): Flow<ApiState<Unit>>
     suspend fun deleteDailyHugg(id: Long): Flow<ApiState<Unit>>
+    suspend fun getDailyHuggList(page: Int): Flow<ApiState<DailyHuggListResponseVo>>
 }
