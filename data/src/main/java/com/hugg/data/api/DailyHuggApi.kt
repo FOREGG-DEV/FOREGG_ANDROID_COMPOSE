a20b665beef6d4c06054e5e22f321a8ae3f89405
dogmania <hj296914@gmail.com>
2024-10-14 01:33:10 +0900

    :sparkles: Feat: 데일리 허그 전체 조회 api 함수 구현

--- data/src/main/java/com/hugg/data/api/DailyHuggApi.kt
+++ data/src/main/java/com/hugg/data/api/DailyHuggApi.kt
@@ -1,6 +1,7 @@
 package com.hugg.data.api
 
 import com.hugg.data.base.ApiResponse
+import com.hugg.data.dto.dailyHugg.DailyHuggListResponse
 import com.hugg.data.dto.dailyHugg.DailyHuggResponse
 import com.hugg.domain.base.ApiState
 import com.hugg.domain.model.request.dailyHugg.CreateDailyHuggVo
@@ -16,6 +17,7 @@ import retrofit2.http.POST
 import retrofit2.http.PUT
 import retrofit2.http.Part
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface DailyHuggApi {
 
@@ -47,4 +49,9 @@ interface DailyHuggApi {
     suspend fun deleteDailyHugg(
         @Path("id") id: Long
     ): Response<ApiResponse<Unit>>
+
+    @GET(Endpoints.DailyHugg.DAILY)
+    suspend fun getDailyHuggList(
+        @Query("page") page: Int
+    ): Response<ApiResponse<DailyHuggListResponse>>
 }
