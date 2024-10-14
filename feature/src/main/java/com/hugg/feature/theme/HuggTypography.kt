42ccb15c8db572864c1e125d9e5a07bcac6837f6
rudtjr1106 <83599356+rudtjr1106@users.noreply.github.com>
2024-10-14 13:39:36 +0900

    refactor - HuggText 스타일 지정

--- feature/src/main/java/com/hugg/feature/theme/HuggTypography.kt
+++ feature/src/main/java/com/hugg/feature/theme/HuggTypography.kt
@@ -9,6 +9,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.hugg.feature.R
 
 @Composable
@@ -138,3 +139,109 @@ fun p5(): TextStyle = TextStyle(
     lineHeight = dpToSp(dp = (11.2).dp),
     fontSize = dpToSp(dp = 8.dp)
 )
+
+object HuggTypography {
+    val btn: TextStyle = TextStyle(
+        fontFamily = huggFontFamily,
+        fontWeight = FontWeight.Bold,
+        letterSpacing = (-0.04).sp,
+        lineHeight = 24.sp,
+        fontSize = 24.sp
+    )
+
+    val h1: TextStyle = TextStyle(
+        fontFamily = huggFontFamily,
+        fontWeight = FontWeight.SemiBold,
+        letterSpacing = (-0.03).sp,
+        lineHeight = 31.sp,
+        fontSize = 24.sp
+    )
+
+    val h2: TextStyle = TextStyle(
+        fontFamily = huggFontFamily,
+        fontWeight = FontWeight.SemiBold,
+        letterSpacing = (-0.03).sp,
+        lineHeight = 28.sp,
+        fontSize = 20.sp
+    )
+
+    val h3: TextStyle = TextStyle(
+        fontFamily = huggFontFamily,
+        fontWeight = FontWeight.SemiBold,
+        letterSpacing = (-0.03).sp,
+        lineHeight = 22.sp,
+        fontSize = 16.sp
+    )
+
+    val h4: TextStyle = TextStyle(
+        fontSize = 14.sp,
+        lineHeight = 19.sp,
+        letterSpacing = (-0.03).sp,
+        fontFamily = huggFontFamily,
+        fontWeight = FontWeight.SemiBold,
+    )
+
+    val p1: TextStyle = TextStyle(
+        fontSize = 16.sp,
+        lineHeight = 22.sp,
+        letterSpacing = (-0.03).sp,
+        fontFamily = huggFontFamily,
+        fontWeight = FontWeight.Medium,
+    )
+
+    val p1_l: TextStyle = TextStyle(
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = (-0.03).sp,
+        fontFamily = huggFontFamily,
+        fontWeight = FontWeight.Light,
+    )
+
+    val p2: TextStyle = TextStyle(
+        fontSize = 14.sp,
+        lineHeight = 19.sp,
+        letterSpacing = (-0.03).sp,
+        fontFamily = huggFontFamily,
+        fontWeight = FontWeight.Medium,
+    )
+
+    val p2_l: TextStyle = TextStyle(
+        fontSize = 14.sp,
+        lineHeight = 19.sp,
+        letterSpacing = (-0.03).sp,
+        fontFamily = huggFontFamily,
+        fontWeight = FontWeight.Light,
+    )
+
+    val p3: TextStyle = TextStyle(
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        letterSpacing = -0.03.sp,
+        fontFamily = huggFontFamily,
+        fontWeight = FontWeight.Medium,
+    )
+
+    val p3_l: TextStyle = TextStyle(
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        letterSpacing = (-0.03).sp,
+        fontFamily = huggFontFamily,
+        fontWeight = FontWeight(300),
+    )
+
+    val p4: TextStyle = TextStyle(
+        fontSize = 10.sp,
+        lineHeight = 10.sp,
+        letterSpacing = (-0.03).sp,
+        fontFamily = huggFontFamily,
+        fontWeight = FontWeight.Medium,
+    )
+
+    val p5: TextStyle = TextStyle(
+        fontSize = 8.sp,
+        lineHeight = (11.2).sp,
+        letterSpacing = (-0.03).sp,
+        fontFamily = huggFontFamily,
+        fontWeight = FontWeight.Normal,
+    )
+}
