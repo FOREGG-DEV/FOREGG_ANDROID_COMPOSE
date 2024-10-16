6b6568dd587da2c04e7b6f0ea082017fe3ba7ce7
dogmania <hj296914@gmail.com>
2024-10-16 10:02:17 +0900

    :sparkles: Feat: Challenge 모듈 생성

---
+++ feature/challenge/build.gradle.kts
@@ -0,0 +1,86 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    kotlin("kapt")
+    id("dagger.hilt.android.plugin")
+    id("kotlin-parcelize")
+}
+
+android {
+    namespace = "com.hugg.challenge"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation(project(":feature"))
+    implementation(project(":domain"))
+
+    implementation(AndroidX.CORE)
+    implementation(AndroidX.APPCOMPAT)
+    implementation(Google.MATERIAL)
+    implementation(AndroidX.NAVIGATION_COMPOSE)
+    implementation(AndroidX.LIFECYCLE_VIEW_MODEL_KTX)
+    implementation(AndroidX.LIFECYCLE_RUNTIME_KTX)
+    implementation(AndroidX.LIFECYCLE_RUNTIME_COMPOSE)
+    implementation(AndroidX.SPLASH)
+    implementation(AndroidX.THREE_TEN)
+    implementation(AndroidX.SWIPE_REFRESH)
+    implementation(AndroidX.DATA_STORE_PREFERENCES)
+    implementation(AndroidX.FRAGMENT_KTX)
+
+    //코루틴
+    implementation(Kotlin.COROUTINES_CORE)
+    implementation(Kotlin.COROUTINES)
+
+    //힐트
+    implementation(Google.HILT_ANDROID)
+    implementation(Google.HILT_CORE)
+    implementation(Google.HILT_COMPOSE)
+    implementation(Google.FCM)
+    implementation(Google.FCM_KTX)
+    implementation(Google.FIREBASE_ANALYTICS)
+    implementation(platform(Google.FIREBASE_BOM))
+
+    implementation(AndroidX.COMPOSE_BOM)
+    implementation(AndroidX.COMPOSE_MATERIAL)
+    implementation(AndroidX.COMPOSE_UI)
+    implementation(AndroidX.COMPOSE_GRAPHICS)
+    implementation(AndroidX.COMPOSE_PREVIEW)
+    implementation(AndroidX.ACTIVITY_COMPOSE)
+    implementation(AndroidX.LIVE_DATA)
+
+    implementation(Libraries.COIL)
+    implementation(Libraries.COIL_COMPOSE)
+
+    kapt(Google.HILT_COMPILER)
+
+    implementation(Libraries.RETROFIT_CONVERTER_GSON)
+
+    implementation(Kotlin.PARCELIZE)
+
+    implementation(Libraries.LOTTIE)
+}
