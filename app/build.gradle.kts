6709630428029c102df0ba7b126f3aefd612df47
rudtjr1106 <83599356+rudtjr1106@users.noreply.github.com>
2024-10-25 11:55:58 +0900

    version update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.hugg.presentation"
         minSdk = 24
         targetSdk = 34
-        versionCode = 22
-        versionName = "2.0.2"
+        versionCode = 23
+        versionName = "2.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
