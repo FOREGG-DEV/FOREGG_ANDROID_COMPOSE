076345c319c1b4f7498636b3517b66a069f6b349
dogmania <hj296914@gmail.com>
2024-11-01 10:53:03 +0900

    :sparkles: Feat: Splash가 실행되고 있을 때 챌린지 닉네임을 설정하는 로직 구현

--- feature/sign/src/main/java/com/hugg/sign/splash/SplashViewModel.kt
+++ feature/sign/src/main/java/com/hugg/sign/splash/SplashViewModel.kt
@@ -2,6 +2,7 @@ package com.hugg.sign.splash
 
 import androidx.lifecycle.viewModelScope
 import com.hugg.domain.model.response.profile.ProfileDetailResponseVo
+import com.hugg.domain.repository.ChallengeRepository
 import com.hugg.domain.repository.ProfileRepository
 import com.hugg.feature.base.BaseViewModel
 import com.hugg.feature.base.PageState
@@ -13,8 +14,13 @@ import javax.inject.Inject
 
 @HiltViewModel
 class SplashViewModel @Inject constructor(
-    private val profileRepository: ProfileRepository
+    private val profileRepository: ProfileRepository,
+    private val challengeRepository: ChallengeRepository
 ) : BaseViewModel<PageState.Default>(PageState.Default) {
+
+    init {
+        getChallengeNicknameFromLocal()
+    }
 
     fun checkLogin(){
         viewModelScope.launch {
@@ -36,5 +42,23 @@ class SplashViewModel @Inject constructor(
 
     private fun goToSign(){
         emitEventFlow(SplashEvent.GoToSignEvent)
+    }
+
+    private fun getChallengeNicknameFromLocal() {
+        viewModelScope.launch {
+            challengeRepository.getNickname().collect { nickname ->
+                if (nickname != null) {
+                    UserInfo.challengeNickname = nickname
+                } else {
+//                    getChallengeNicknameFromServer()
+                }
+            }
+        }
+    }
+
+    private fun getChallengeNicknameFromServer() {
+        viewModelScope.launch {
+            // TODO 서버에서 챌린지 닉네임 가져오는 로직
+        }
     }
 }
