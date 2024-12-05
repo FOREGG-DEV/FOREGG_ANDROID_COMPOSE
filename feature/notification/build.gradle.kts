abc1b6124530cf4d15dfad02e6d0eb571d109483
rudtjr1106 <83599356+rudtjr1106@users.noreply.github.com>
2024-12-05 13:44:16 +0900

    feat - notification 모듈 추가

---
+++ feature/notification/build.gradle.kts
@@ -0,0 +1,75 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    kotlin("kapt")
+    id("dagger.hilt.android.plugin")
+}
+
+android {
+    namespace = "com.hugg.notification"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.14"
+    }
+    packagingOptions {
+        exclude("META-INF/gradle/incremental.annotation.processors")
+    }
+}
+
+dependencies {
+    implementation(project(":feature"))
+    implementation(project(":domain"))
+    implementation(AndroidX.CORE)
+    implementation(AndroidX.APPCOMPAT)
+    implementation(Google.MATERIAL)
+    implementation(AndroidX.NAVIGATION_COMPOSE)
+    implementation(AndroidX.LIFECYCLE_VIEW_MODEL_KTX)
+    implementation(AndroidX.LIFECYCLE_RUNTIME_KTX)
+    implementation(AndroidX.LIFECYCLE_RUNTIME_COMPOSE)
+
+    //코루틴
+    implementation(Kotlin.COROUTINES_CORE)
+    implementation(Kotlin.COROUTINES)
+
+    //힐트
+    implementation(Google.HILT_ANDROID)
+    implementation(Google.HILT_CORE)
+    implementation(Google.HILT_COMPOSE)
+    implementation(Google.FIREBASE_ANALYTICS)
+    implementation(platform(Google.FIREBASE_BOM))
+
+    implementation(AndroidX.COMPOSE_BOM)
+    implementation(AndroidX.COMPOSE_MATERIAL)
+    implementation(AndroidX.COMPOSE_UI)
+    implementation(AndroidX.COMPOSE_GRAPHICS)
+    implementation(AndroidX.COMPOSE_PREVIEW)
+    implementation(AndroidX.ACTIVITY_COMPOSE)
+
+    kapt(Google.HILT_COMPILER)
+}
