aa9550b006cdb2ef9d89fe08530996ae4ca5ec63
seok <rudtjr1206@naver.com>
2025-11-26 10:09:10 +0900

    version up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
         applicationId = "com.hugg.presentation"
         minSdk = 24
         targetSdk = 35
-        versionCode = 10
+        versionCode = 11
         versionName = "1.0.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
